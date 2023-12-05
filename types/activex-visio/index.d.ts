1070b4f8fa2a89ff89fa48fd5a2902678f3d56bd
threatcon <44587459+threatcon@users.noreply.github.com>
2023-12-04 23:12:23 -0600

    framework

--- types/activex-visio/index.d.ts
+++ types/activex-visio/index.d.ts
@@ -5,6 +5,38 @@
 /// <reference types="activex-dao" />
 
 declare namespace Visio {
+
+    class Application {
+        private "Visio.Application_typekey": Application;
+        private constructor();
+        readonly _Default: string;
+        readonly Application: Application;
+        readonly ActiveDocument: Document;
+        readonly ActivePage: Page;
+        ActivePrinter: string;
+        readonly ActiveWindow: Window;
+        readonly Name: string;
+
+    }
+
+    class Document {
+        private "Visio.Document_typekey": Document;
+        private constructor();
+    }
+
+    interface Documents {
+        /** @param TextQualifier [TextQualifier=1] */
+    }
+
+    class Page {
+        private "Visio.Page_typekey": Page;
+        private constructor();
+       }
+
+    interface ActiveXObjectNameMap {
+        "Visio.Application": Visio.Application;
+        "Visio.Document": Visio.Document;
+        "Visio.Page": Visio.Page;
+    }
    
-    
 }
